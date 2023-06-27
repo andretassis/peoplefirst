5a8909eb4ff52a5f99cc3b4c07af1938d60a5b71
andretassis <tassisandre@gmail.com>
2023-06-27 15:17:30 -0300

    Remoção do corSecundaria do css e criação e estilização de um h3

--- src/App.js
+++ src/App.js
@@ -3,38 +3,34 @@ import Banner from './componentes/Banner/Banner';
 import Formulario from './componentes/Formulario/Formulario';
 import Departamentos from './componentes/Departamentos/Departamentos';
 import Rodape from './componentes/Rodape/Rodape';
+import './App.css'
 
 function App() {
 
   const [departamentos, setDepartamentos] = useState([
     {
       nome: "Recrutamento e Seleção",
-      corPrimaria: "#57C278",
-      corSecundaria: "#D9F7E9",
+      corPrimaria: "#57C278"
     },
 
     {
       nome: "Treinamento e Desenvolvimento",
       corPrimaria: "#82CFFA",
-      corSecundaria: "#E8F8FF",
     },
 
     {
       nome: "Gestão de Talentos",
       corPrimaria: "#A6D157",
-      corSecundaria: "#F0F8E2",
     },
 
     {
       nome: "Administração de Pessoal",
       corPrimaria: "#E06B69",
-      corSecundaria: "#FDE7E8",
     },
 
     {
       nome: "Comunicação Interna",
       corPrimaria: "#D86EBF",
-      corSecundaria: "#FAE5F5",
     }
   ])
 
@@ -61,16 +57,17 @@ function App() {
     <div className="App">
       <Banner />
       <Formulario departamentos={departamentos.map(departamento => departamento.nome)} novoColaborador={colaborador => novoCadastro(colaborador)} />
-
-      {departamentos.map(departamento => <Departamentos
-        key={departamento.nome}
-        nome={departamento.nome}
-        corPrimaria={departamento.corPrimaria}
-        corSecundaria={departamento.corSecundaria}
-        colaboradores={colaboradores.filter(colaborador => colaborador.departamento === departamento.nome)}
-        deletar={deletarColaborador}
-        mudarCor={mudarCorDoColaborador}
-      />)}
+      <section className="organizacao">
+        <h3>Organização</h3>
+        {departamentos.map(departamento => <Departamentos
+          key={departamento.nome}
+          nome={departamento.nome}
+          corPrimaria={departamento.corPrimaria}
+          colaboradores={colaboradores.filter(colaborador => colaborador.departamento === departamento.nome)}
+          deletar={deletarColaborador}
+          mudarCor={mudarCorDoColaborador}
+        />)}
+      </section>
 
       <Rodape />
     </div>
