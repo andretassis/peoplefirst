daf4e61d243917e911c1f962675903ecc422f03a
Andre Tassis <tassisandre@gmail.com>
2023-06-29 12:09:33 -0300

    criada feature de favoritar colaborador

--- src/componentes/Departamentos/Departamentos.js
+++ src/componentes/Departamentos/Departamentos.js
@@ -6,19 +6,27 @@ import './Departamentos.css'
 const Departamentos = (props) => {
     return (
         (props.colaboradores.length > 0) && <section className="departamentos" style={{ backgroundColor: hexToRgba(props.corPrimaria, '0.2') }} >
-            <input type="color" className="input-cor" value={props.corPrimaria} onChange={elemento => props.mudarCor(elemento.target.value, props.id)} />
+            <input
+                type="color"
+                className="input-cor"
+                value={props.corPrimaria}
+                onChange={elemento => props.mudarCor(elemento.target.value, props.id)}
+            />
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
             <div className="cards">
                 {props.colaboradores.map((colaborador) => {
-                    return <Colaborador
-                        key={colaborador.nome}
-                        id={colaborador.id}
-                        nome={colaborador.nome}
-                        cargo={colaborador.cargo}
-                        imagem={colaborador.imagem}
-                        corDeFundo={props.corPrimaria}
-                        deletar={props.deletar}
-                    />
+                    return (
+                        <Colaborador
+                            key={colaborador.nome}
+                            id={colaborador.id}
+                            nome={colaborador.nome}
+                            cargo={colaborador.cargo}
+                            imagem={colaborador.imagem}
+                            corDeFundo={props.corPrimaria}
+                            deletar={props.deletar}
+                            marcarFavorito={props.marcarFavorito}
+                        />
+                    )
                 })}
             </div>
         </section>
